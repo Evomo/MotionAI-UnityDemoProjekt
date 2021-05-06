f502442a5ccd2af01d3cb6d44fda226cde90b2d6
Jakob Wowy <jakob.wowy@evomo.de>
2021-05-07 01:26:48 +0200

    First commit

---
+++ Assets/Scripts/DemoInputManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MotionAI.Core.Controller;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Color;
+
+public class DemoInputManager : MonoBehaviour
+{
+    
+    public Button SquatIndicator;
+    public Button PushUpIndicator;
+    
+    [HideInInspector] public MotionAIManager maim;
+
+    private void Start()
+    {
+        maim = GetComponent<MotionAIManager>();
+    }
+    IEnumerator TriggerButtonCoroutine(Button button)
+    {
+        button.image.color = Color.green;
+        
+        yield return new WaitForSeconds(1);
+        
+        button.image.color = Color.gray;
+    }
+    public void TriggerSquatIndicator()
+    {
+        StartCoroutine(TriggerButtonCoroutine(SquatIndicator));
+    }
+    
+    public void TriggerPushUpIndicator()
+    {
+        StartCoroutine(TriggerButtonCoroutine(PushUpIndicator));
+    }
+    
+    public void ScanForMovesense()
+    {
+        Debug.Log("ScanForMovesense");
+    }
+    
+    public void StartTracking()
+    {
+        Debug.Log("StartTracking");
+        if (!maim.isTracking) {
+            maim.StartTracking();
+        }
+    }
+    
+    public void StopTracking()
+    {
+        Debug.Log("StopTracking");
+        if (maim.isTracking)
+        {
+            maim.StopTracking();
+        }
+    }
+}
---
+++ Assets/Scripts/DemoMotionAIController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MotionAI.Core.Controller;
+using MotionAI.Core.Models;
+using MotionAI.Core.Models.Generated;
+using MotionAI.Core.POCO;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class ElmoEvents
+{
+    public OnElmoEvent squat, pushUp;
+}
+
+public class DemoMotionAIController : MotionAIController
+{
+    private Bodyweight _movementModel;
+    
+    public TMP_Text LastElementalMovement;
+    
+    public ElmoEvents movements = new ElmoEvents();
+
+
+    //Don't forget to override! Or the events won't work
+    public override void Start()
+    {
+        base.Start();
+        Debug.Log("Starting inspector controller");
+
+        //Initialize the controller by casting the model to the controller you wish to use
+        _movementModel = GetComponent<AbstractModelComponent>() as Bodyweight;
+/*
+        //Once initialized you can access its members and events!
+
+        if (_movementModel != null) {
+            _movementModel.moves?.jumping_jack.onMove.AddListener(JJCallBack);
+        }
+
+        //In this particular example I want to subscribe to ALL events except duck!
+        foreach (MoveHolder moveHolder in modelManager.model.GetMoveHolders()) {
+            if (moveHolder.id != MovementEnum.duck) {
+                moveHolder.onMove.AddListener(AllEventsCallback);
+            }
+        }*/
+    }
+
+    public void debugAddElmo()
+    {
+        ElementalMovement elmo = new ElementalMovement
+        {
+            typeLabel = "squat_down", typeID = ElmoEnum.squat_down, rejected = false
+        };
+
+        HandleElmo(elmo);
+    }
+
+    protected override void HandleMovement(EvoMovement msg)
+    {
+        msg.elmos.ForEach(HandleElmo);
+    }
+
+    private void HandleElmo(ElementalMovement elementalMovement)
+    {
+        if (elementalMovement.typeID == ElmoEnum.squat_down)
+        {
+            movements.squat.Invoke(elementalMovement);
+        }
+        else if (elementalMovement.typeID == ElmoEnum.pushup_down)
+        {
+            movements.pushUp.Invoke(elementalMovement);
+        }
+
+        LastElementalMovement.text = elementalMovement.typeLabel;
+    }
+}
